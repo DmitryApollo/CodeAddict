cba613d8096e7e05a4f8cc3dbce8d78bfbfe712a
Дмитрий <dmitryvysocki98@gmail.com>
2020-12-08 21:54:42 +0100

    init

---
+++ CodeAddict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeAddict
+//
+//  Created by Дмитрий on 08/12/2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
